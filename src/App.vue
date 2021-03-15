95a7b8133de54b12e7cb1ac3e731b2f5a20a30e2
Matus Poruben <poruben.matus@gmail.com>
2021-03-16 00:42:41 +0100

    setup: init setup

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,38 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div>
+    <div>
+
+      <input placeholder="test"/>
+      <input placeholder="test"/>
+
+      <GeneratePassword />
+
+      <button>Odoslat</button>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import { defineComponent } from 'vue'
+
+import GeneratePassword from './plugins/password/GeneratePassword.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    GeneratePassword
   }
-});
+
+
+})
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="sass">
+#app
+  font-family: Avenir, Helvetica, Arial, sans-serif
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
+  text-align: center
+  color: #2c3e50
+  margin-top: 60px
 </style>
---
+++ src/plugins/password/GeneratePassword.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <button>Generate</button>
+    
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  
+})
+
+</script>
+
+<style scoped lang="sass">
+
+</style>
