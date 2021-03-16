10f4a36a484fd895d51ff69d789e79348d6b4e1e
Matus Poruben <poruben.matus@gmail.com>
2021-03-16 17:35:57 +0100

    feature: password form

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,24 @@
 <template>
-  <div>
-    <div>
-
-      <input placeholder="test"/>
-      <input placeholder="test"/>
-
-      <GeneratePassword />
-
-      <button>Odoslat</button>
+  <div id="app">
+    <div class="card">
+      <h2>Password Form</h2>
+      <div class="formItem">
+        <label>Password</label>
+        <input v-model="password" :type="passwordType" />
+      </div>
+      <div class="formItem">
+        <label>Password Confirmation</label>
+        <input v-model="passwordConfirmation" :type="passwordType" />
+      </div>
+      <div>
+        <button class="m-1" @click="togglePasswordVisibility">
+          {{ (passwordVisibility) ? 'Hide password' : 'Show password' }}
+        </button>
+        <generate-password class="m-1" @generated="passwordGenerated" />
+      </div>
+      <div class="formItem">
+        <button class="m-1">Submit</button>
+      </div>
     </div>
   </div>
 </template>
@@ -18,21 +29,57 @@ import { defineComponent } from 'vue'
 import GeneratePassword from './plugins/password/GeneratePassword.vue'
 
 export default defineComponent({
-  name: 'App',
+
   components: {
     GeneratePassword
-  }
+  },
 
+  data() { return {
+    password: '',
+    passwordConfirmation: '',
+    passwordVisibility: false
+  }},
+
+  computed: {
+    passwordType() {
+      return this.passwordVisibility ? 'text': 'password'
+    }
+  },
+
+  methods: {
+    togglePasswordVisibility() {
+      this.passwordVisibility = !this.passwordVisibility
+    },
+
+    passwordGenerated(pass: string) {
+      this.password = pass
+      this.passwordConfirmation = pass
+      this.passwordVisibility = true
+    },
+  }
 
 })
 </script>
 
-<style lang="sass">
+<style lang="sass" scoped>
 #app
-  font-family: Avenir, Helvetica, Arial, sans-serif
-  -webkit-font-smoothing: antialiased
-  -moz-osx-font-smoothing: grayscale
   text-align: center
-  color: #2c3e50
-  margin-top: 60px
+  height: 100vh
+  background: #222222
+  display: flex
+  justify-content: center
+  align-items: center
+  .card
+    background: #111111
+    padding: 10px 30px
+    border-radius: 10px
+    width: 260px
+    color: white
+    .formItem
+      display: flex
+      flex-direction: column
+      align-items: stretch
+      justify-content: stretch
+      text-align: left
+      margin: 10px 0px
 </style>
